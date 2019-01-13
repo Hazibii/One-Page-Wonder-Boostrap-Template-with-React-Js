624b7d07d2bad7f2ddd665e9c9118478aad8b991
Hasib Sarvari <tkdparallax@gmail.com>
2019-01-13 16:33:09 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/header';
+import Navigation from './component/nav';
+import SectionOne from './component/sectionOne';
+import SectionTwo from './component/sectionTwo';
+import SectionThree from './component/sectionThree';
+import Footer from './component/footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header /> 
+        <Navigation />
+        <SectionOne />
+        <SectionTwo /> 
+        <SectionThree />
+        <Footer /> 
       </div>
     );
   }
---
+++ src/component/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+const Footer = () => (    
+
+    <footer className="py-5 bg-black">
+      <div className="container">
+        <p className="m-0 text-center text-white small">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+   
+);
+
+export default Footer;
