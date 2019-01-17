13df752c593166cf9d1c8b5e1bbedba81f7cbe7c
Hasib Sarvari <tkdparallax@gmail.com>
2019-01-17 20:11:08 +0800

    Section Image Update

--- src/component/sectionOne.js
+++ src/component/sectionOne.js
@@ -8,7 +8,7 @@ const SectionOne = () => (
         <div className="row align-items-center">
           <div className="col-lg-6 order-lg-2">
             <div className="p-5">
-              <img className="img-fluid rounded-circle" src="https://via.placeholder.com/150" alt="" />
+              <img className="img-fluid rounded-circle" src="./assets/img/01.jpg" alt="" />
             </div>
           </div>
           <div className="col-lg-6 order-lg-1">
--- src/component/sectionTwo.js
+++ src/component/sectionTwo.js
@@ -8,7 +8,7 @@ const SectionOne = () => (
       <div className="row align-items-center">
         <div className="col-lg-6">
           <div className="p-5">
-            <img className="img-fluid rounded-circle" src="https://via.placeholder.com/150" alt=""/>
+            <img className="img-fluid rounded-circle" src="./assets/img/02.jpg" alt=""/>
           </div>
         </div>
         <div className="col-lg-6">
